4e66985130da809b0682a7e76e272842fef1b563
dlrjswns <leekyunjun@gmail.com>
2025-08-08 17:50:53 +0900

    [INIT] EgenBoys프로젝트 생성
    - SwiftUI 사용

---
+++ EgenBoys/EgenBoys/EgenBoysApp.swift
@@ -0,0 +1,17 @@
+//
+//  EgenBoysApp.swift
+//  EgenBoys
+//
+//  Created by 이건준 on 8/8/25.
+//
+
+import SwiftUI
+
+@main
+struct EgenBoysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
