c24fc4e21f668e385830c410515fded0da1d4fcf
dlrjswns <leekyunjun@gmail.com>
2025-08-11 11:54:44 +0900

    [FEAT] QuizItemView 구현
    - 퀴즈 List표현을 위한 ItemView 구현
    
    quizitemview 스쿼시커밋

---
+++ EgenBoys/EgenBoys/Source/Feature/QuizList/CustomView/QuizItemView.swift
@@ -0,0 +1,51 @@
+//
+//  QuizItemView.swift
+//  EgenBoys
+//
+//  Created by 이건준 on 8/11/25.
+//
+
+import SwiftUI
+
+struct QuizItem: Identifiable {
+    let id = UUID()
+    let title: String
+    let description: String
+    let imageURL: URL?
+    let videoURL: URL?
+}
+
+struct QuizItemView: View {
+    @Binding var item: QuizItem
+    
+    var body: some View {
+        HStack {
+            if let imageURL = item.imageURL {
+                AsyncImage(url: imageURL) { image in
+                    image.resizable()
+                        .scaledToFill()
+                        .frame(width: 80, height: 80)
+                        .clipShape(RoundedRectangle(cornerRadius: 15))
+                        .shadow(radius: 5)
+                } placeholder: {
+                    ProgressView()
+                        .progressViewStyle(CircularProgressViewStyle(tint: .blue))
+                        .scaleEffect(1.5)
+                }
+                .frame(width: 80, height: 80)
+            }
+            
+            VStack(alignment: .leading, spacing: 8) {
+                Text(item.title)
+                    .font(.headline)
+                    .foregroundColor(.primary)
+                
+                Text(item.description)
+                    .font(.subheadline)
+                    .foregroundColor(.gray)
+            }
+            .frame(maxWidth: .infinity, alignment: .leading)
+        }
+        .padding()
+    }
+}
