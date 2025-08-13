0897778eb8b44f8b3baaf69652ba4e1e329b3738
dlrjswns <leekyunjun@gmail.com>
2025-08-13 09:21:34 +0900

    [FEAT] 정의한 모델 컨테이너 App 추가
    - Quiz, Question

--- EgenBoys/EgenBoys/Application/EgenBoysApp.swift
+++ EgenBoys/EgenBoys/Application/EgenBoysApp.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import SwiftData
 
 @main
 struct EgenBoysApp: App {
@@ -13,5 +14,6 @@ struct EgenBoysApp: App {
         WindowGroup {
             MainTabView()
         }
+        .modelContainer(for: [Quiz.self, Question.self])
     }
 }
